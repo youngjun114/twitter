2366512e815e0c09260a7ae60233d7871fc22278
Youngjun Woo <youngjun_92@hotmail.com>
2021-07-27 02:56:38 -0700

    added index page

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
 import Header from './components/header/header';
 import Tweets from './components/tweets/tweets';
+import Index from './pages/index';
+
 function App({ tweetService }) {
   return (
     <>
-      <Header />
-      <Tweets tweetService={tweetService} />
+      <Switch>
+        <Route exact path='/'>
+          <Index />
+        </Route>
+        <Route path='/home'>
+          <Header />
+          <Tweets tweetService={tweetService} />
+        </Route>
+      </Switch>
     </>
   );
 }
